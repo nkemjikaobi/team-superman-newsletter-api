6942a5df9cb48260e710022abbd070c09b86cf91
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2020-06-11 16:10:11 +0100

    removed swagger docs

--- SubscriptionService/src/middlewares/JWTMiddleware.js
+++ SubscriptionService/src/middlewares/JWTMiddleware.js
@@ -1,25 +1,25 @@
-const expressJWT =require('express-jwt');
+// const expressJWT =require('express-jwt');
 
-const checkToken = expressJWT({
-  secret: process.env.JWT || 'SomeJuicySecretSetOnEnv'
-});
+// const checkToken = expressJWT({
+//   secret: process.env.JWT || 'SomeJuicySecretSetOnEnv'
+// });
 
-/** JWT Middleware class */
-class JWT {
-  /**
-   * Decodes token with app secret
-   * @returns {object} next with request
-   */
-  static decodeToken() {
-    return (req, res, next) => {
-      //  Checks if token was passed on url
-      const query = { req };
-      if (query && query.hasOwnProperty('bearerToken')) {
-        req.headers.authorization = `Bearer ${query.bearerToken}`;
-      }
-      checkToken(req, res, next);
-    };
-  }
-}
+// /** JWT Middleware class */
+// class JWT {
+//   /**
+//    * Decodes token with app secret
+//    * @returns {object} next with request
+//    */
+//   static decodeToken() {
+//     return (req, res, next) => {
+//       //  Checks if token was passed on url
+//       const query = { req };
+//       if (query && query.hasOwnProperty('bearerToken')) {
+//         req.headers.authorization = `Bearer ${query.bearerToken}`;
+//       }
+//       checkToken(req, res, next);
+//     };
+//   }
+// }
 
-module.exports = JWT;
+// module.exports = JWT;
